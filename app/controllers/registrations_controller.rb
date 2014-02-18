2d9e71e63ec4eeebbff8bc386bd77bd01400361b
Jonathan Pichot <jonathan@pichot.us>
2014-02-18 14:12:00 -0500

    Add controller test on user sign up with Devise helpers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,7 +2,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     user = User.new(user_params)
 
-    if user.save!(user_params)
+    if user.save(user_params)
       redirect_to :dashboard
     else
       @user = user
