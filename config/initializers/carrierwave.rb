21cd620c2d93f93042015afba1ca4bbafb8c1500
dcarpenter88 <dcarpenter88@mac.com>
2014-03-26 16:35:08 -0400

    Add fog gem to suport S3.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,17 @@
-if Rails.env.test?
-  CarrierWave.configure do |config|
-    config.storage = :file
+CarrierWave.configure do |config|
+  credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+  }
+  fog_attributes = {'Cache-Control' => 'max-age=315576000, public'}
+  fog_attributes['x-amz-storage-class'] = 'REDUCED_REDUNDANCY' unless Rails.env.production?
+
+  if Rails.env.test?
+    Fog.mock!
+    connection = Fog::Storage.new(credentials)
+    connection.directories.create(:key => ENV["AWS_S3_IMAGE_BUCKET"])
+
     config.enable_processing = false
   end
 end
