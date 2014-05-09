efc6f6af8123d1889e9654ed0d9a7c61b324f44c
Ryan Glover <ersatzryan@gmail.com>
2014-05-09 07:53:47 -0400

    Do Event fetching in the controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,15 +1,31 @@
 class EventsController < ApplicationController
   def index
-    interactor = Events::Fetch.perform(date: event_params[:date])
+    events = Event.where(audition_date: query_date_range).order("audition_date ASC")
 
     if current_user.present?
-      render json: interactor.events, meta: {member: true, admin: current_user.admin?}
+      render json: events, meta: {member: true, admin: current_user.admin?}
     else
-      render json: interactor.events, meta: {member: false, admin: false}, each_serializer: LimitedEventSerializer
+      render json: events, meta: {member: false, admin: false}, each_serializer: LimitedEventSerializer
     end
   end
 
-  def event_params
-    params.permit(date: [:start, :end])
+  private
+
+  def query_date_range
+    params[:date] ||= {}
+    start_date = DateTime.parse(params[:date].fetch(:start, Time.now.in_time_zone("UTC").beginning_of_month.to_s))
+    end_date = DateTime.parse(params[:date].fetch(:end, Time.now.in_time_zone("UTC").end_of_month.to_s))
+
+    range_for(start_date, end_date)
+  end
+
+  def range_for(start_date, end_date)
+    if end_date < 60.days.ago
+      nil
+    elsif start_date < 60.days.ago && end_date > 60.days.ago
+      60.days.ago..end_date
+    else
+      start_date..end_date
+    end
   end
 end
--- app/interactors/events/query_events.rb
+++
@@ -1,9 +0,0 @@
-module Events
-  class QueryEvents
-    include Interactor
-
-    def perform
-      context[:events] = Event.where(audition_date: (query_date_range)).order("audition_date ASC")
-    end
-  end
-end
