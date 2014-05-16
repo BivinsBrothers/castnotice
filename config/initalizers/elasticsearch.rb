1c81a9abbb73d8546d8cfbe921abbbd735af376c
Jonathan Pichot <jonathan@pichot.us>
2014-05-16 14:40:23 -0400

    Add Elasticsearch to Resume model

---
+++ config/initalizers/elasticsearch.rb
@@ -0,0 +1,7 @@
+if Rails.env.staging && Rails.env.production
+  Elasticsearch::Model.client = Elasticsearch::Client.new(hosts: [
+    { host: Figaro.env.searchbox_ssl_url,
+      port: "443",
+      scheme: "https"
+    } ])
+end
