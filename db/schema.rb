23bce795ac25fd096adc9455482692698c2860c5
dcarpenter88 <dcarpenter88@mac.com>
2014-05-27 09:28:12 -0400

    Fix schema changes with rebasing master

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20140522173528) do
-=======
-ActiveRecord::Schema.define(version: 20140521205234) do
->>>>>>> Talent can submit critique requests.
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
