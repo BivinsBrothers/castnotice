c739917dd7a14bf61e7774a24b45401c2920bdd5
Jonathan Pichot <jonathan@pichot.us>
2014-05-29 18:04:58 -0400

    Improve ConversationDecorator

--- app/decorators/conversation_decorator.rb
+++ app/decorators/conversation_decorator.rb
@@ -1,15 +1,11 @@
 class ConversationDecorator < Draper::Decorator
   delegate_all
 
-  def unread_messages_for?
+  def has_unread_messages?
     Message.unread_for?(context[:user], self)
   end
 
  def correspondent
-    if sender_id == context[:user].id
-      recipient
-    else
-      sender
-    end
+   self.other_correspondent_for(context[:user])
   end
 end
