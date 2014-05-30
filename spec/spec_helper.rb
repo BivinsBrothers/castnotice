881a42b0cd247e4247983f12e924393281a5e4b9
Andrew Bredow <bredow@gmail.com>
2014-05-30 11:47:20 -0400

    Add cached files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ Billy.configure do |c|
   c.cache = true
   c.persist_cache = true
   c.ignore_cache_port = true
-  c.ignore_params = [ "https://js.stripe.com/v2", "https://api.stripe.com" ]
+  c.ignore_params = [ "https://js.stripe.com/v2", "https://api.stripe.com/v1/tokens" ]
   c.non_whitelisted_requests_disabled = false # disable new HTTP connections when true
   c.cache_path = "spec/billy_cache"
 end
