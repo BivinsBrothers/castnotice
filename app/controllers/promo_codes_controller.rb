e3660f7ce45f0cffe9b549bdcb2c2cd4075fd044
Andrew Bredow <bredow@gmail.com>
2014-06-10 11:54:50 -0400

    Add final promo codes
    Also, append the promo to the URL so they can segment their signups
    according to promo codes.

--- app/controllers/promo_codes_controller.rb
+++ app/controllers/promo_codes_controller.rb
@@ -1,8 +1,8 @@
 class PromoCodesController < ApplicationController
   def create
-    if params[:promo_code] == "breakthrough"
+    if ["BreakThru2014", "YoungActors2014"].include?(params[:promo_code])
       session[:allow_breakthrough_promo] = true
-      redirect_to new_user_registration_path(account_type: Stripe::Plans::BROADWAY)
+      redirect_to new_user_registration_path(account_type: Stripe::Plans::BROADWAY, promo: params[:promo_code])
     else
       redirect_to root_path
     end
