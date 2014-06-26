99d337ed9aa0cdae80b3f75de282d723297dc432
dcarpenter88 <dcarpenter88@mac.com>
2014-06-26 12:55:09 -0400

    Change company_phone to new_phone and change to string.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140626131721) do
+ActiveRecord::Schema.define(version: 20140626163720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -150,6 +150,7 @@ ActiveRecord::Schema.define(version: 20140626131721) do
     t.string  "artistic_organizations"
     t.integer "user_id"
     t.text    "biography"
+    t.string  "new_phone"
   end
 
   create_table "messages", force: true do |t|
