1c5b7be1cd62eecd25dadfe714af062010ec6608
Thais Camilo & Pavel Pravosud <dev+narwen+rwz@hashrocket.com>
2015-04-01 10:40:45 -0400

    Bug fix: stop devise's infinite redirect

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -1,3 +1,8 @@
 class PasswordsController < Devise::PasswordsController
   skip_before_action :enforce_promo_code_access
+
+  protected
+  def after_resetting_password_path_for(resource)
+    signed_in_root_path(resource)
+  end
 end
