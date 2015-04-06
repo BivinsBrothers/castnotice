1f8afff2781de8f1b597c06e741d7d89c07f7adb
Thais Camilo & Pavel Pravosud <dev+narwen+rwz@hashrocket.com>
2015-04-06 15:24:45 -0400

    Expire assets on stage

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -33,7 +33,7 @@ Castnotice::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.0.1'
 
   config.assets.initialize_on_precompile = false
 
