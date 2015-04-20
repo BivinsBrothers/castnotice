cc8cc77ffc3cd4ea828eabf0d39c3351fcd1e28b
Thais Camilo & Pavel Pravosud <dev+narwen+rwz@hashrocket.com>
2015-04-20 12:44:12 -0400

    Send emails to campers by camp code

--- app/models/campers_list.rb
+++ app/models/campers_list.rb
@@ -1,3 +1,14 @@
 class CampersList < ActiveRecord::Base
 
+  scope :by_code, proc { |code| where(code: code) }
+
+  def self.send_invitation(code)
+    campers = CampersList.by_code(code)
+    campers.each do |camper|
+      email = camper.user_email
+      link = "http://www.castnotice.com/camper_registration/new?order_id=#{camper.order_id}"
+
+      Notifier.invite_campers('thais@hashrocket.com', link).deliver
+    end
+  end
 end
