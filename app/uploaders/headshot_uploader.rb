67b9f743323fa7e0226351cae9c98a298bab99c5
Thais Camilo & Pavel Pravosud <dev+narwen+rwz@hashrocket.com>
2015-05-05 14:39:33 -0400

    Change headshots directories on S3

--- app/uploaders/headshot_uploader.rb
+++ app/uploaders/headshot_uploader.rb
@@ -26,6 +26,10 @@ class HeadshotUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [300, 300]
   end
 
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
   def auto_orient
     manipulate! do |img|
       img.auto_orient
